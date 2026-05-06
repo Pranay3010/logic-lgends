c6bb14908068aeaa242adbe46d274f952654951c
Pranay3010 <pranayrathore30@gmail.com>
2026-05-06 06:15:42 +0000

    class

--- project/Eco_sim.py
+++ project/Eco_sim.py
@@ -2,6 +2,27 @@ class Organism:
     def __init__(self):
         pass
 
-class plant(Organism):
+    def energy(self):
+        pass
 
+class plant(Organism):
+    
+    def energy(self):
+        pass
+
+
+class animal(Organism):
+
+    def energy(self):
+        pass
+
+class herbivore(animal):
+
+    def energy(self):
+        pass
+
+class omnivore(animal):
+
+    def energy(self):
+        pass
     
